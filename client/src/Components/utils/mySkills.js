92fca0646120b1cf17e805709333b2f06b39c816
alaabadra <alaabadra4@gmail.com>
2019-06-14 19:20:39 +0300

    file inside it data

---
+++ client/src/Components/utils/mySkills.js
@@ -0,0 +1,74 @@
+export default [
+    {
+      id: 1,
+      imageUrl:
+        'https://cdn5.f-cdn.com/ppic/122757424/logo/29757229/k0Dug/profile_logo_.png',
+      fullName: 'Xiaogang Smith',
+      profession: 'FullStack Web/Desktop Application Developer',
+      text:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    },
+    {
+        id: 1,
+        imageUrl:
+          'https://cdn5.f-cdn.com/ppic/122757424/logo/29757229/k0Dug/profile_logo_.png',
+        fullName: 'Xiaogang Smith',
+        profession: 'FullStack Web/Desktop Application Developer',
+        text:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      },
+      {
+        id: 1,
+        imageUrl:
+          'https://cdn5.f-cdn.com/ppic/122757424/logo/29757229/k0Dug/profile_logo_.png',
+        fullName: 'Xiaogang Smith',
+        profession: 'FullStack Web/Desktop Application Developer',
+        text:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      },
+      {
+        id: 1,
+        imageUrl:
+          'https://cdn5.f-cdn.com/ppic/122757424/logo/29757229/k0Dug/profile_logo_.png',
+        fullName: 'Xiaogang Smith',
+        profession: 'FullStack Web/Desktop Application Developer',
+        text:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      },
+      {
+        id: 1,
+        imageUrl:
+          'https://cdn5.f-cdn.com/ppic/122757424/logo/29757229/k0Dug/profile_logo_.png',
+        fullName: 'Xiaogang Smith',
+        profession: 'FullStack Web/Desktop Application Developer',
+        text:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      },
+      {
+        id: 1,
+        imageUrl:
+          'https://cdn5.f-cdn.com/ppic/122757424/logo/29757229/k0Dug/profile_logo_.png',
+        fullName: 'Xiaogang Smith',
+        profession: 'FullStack Web/Desktop Application Developer',
+        text:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      },
+      {
+        id: 1,
+        imageUrl:
+          'https://cdn5.f-cdn.com/ppic/122757424/logo/29757229/k0Dug/profile_logo_.png',
+        fullName: 'Xiaogang Smith',
+        profession: 'FullStack Web/Desktop Application Developer',
+        text:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      },
+      {
+        id: 1,
+        imageUrl:
+          'https://cdn5.f-cdn.com/ppic/122757424/logo/29757229/k0Dug/profile_logo_.png',
+        fullName: 'Xiaogang Smith',
+        profession: 'FullStack Web/Desktop Application Developer',
+        text:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      },
+]
