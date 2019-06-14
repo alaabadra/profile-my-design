3406022a1855bf09684a0fe20756c75774953ff7
alaabadra <alaabadra4@gmail.com>
2019-06-14 18:38:16 +0300

    add info in file in utils

---
+++ client/src/Components/utils/aboutMeFile.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        title :'engineering',
+        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
+        status :'completed'
+    },
+    {
+        id: 2,
+        title :'engineering',
+        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
+        status :'finished'
+    },
+    {
+        id: 3,
+        title :'engineering',
+        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
+        status :'finished'
+    },
+    {
+        id: 4,
+        title :'engineering',
+        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
+        status :'finished'
+    },
+    {
+        id: 5,
+        title :'engineering',
+        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
+        status :'finished'
+    },
+    {
+        id: 6,
+        title :'engineering',
+        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
+        status :'active'
+    },
+]
