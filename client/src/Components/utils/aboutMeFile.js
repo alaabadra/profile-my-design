12bd8f4a36cba2d621ad7d8d6c1b2830eeacce1b
alaabadra <alaabadra4@gmail.com>
2019-06-14 21:09:59 +0300

    edit in info that iniside file js

--- client/src/Components/utils/aboutMeFile.js
+++ client/src/Components/utils/aboutMeFile.js
@@ -1,38 +1,38 @@
 export default [
     {
         id: 1,
-        title :'engineering',
-        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
-        status :'completed'
+        title :'specailization',
+        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section ',
+        status :'finished'
     },
     {
         id: 2,
-        title :'engineering',
-        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
+        title :'working',
+        description :' I was nominated to work for UNRWA for a year in computer engineering , and i worked in Al Hams company in the field of programming, especially programming the web using html, css, bootstrap, PHP  ,So I developed a website using PHP was the entry and exit system for employees in the company and I also helped my colleague design and programmed a general site entitled the company by using html,css,bootstrap,php ',
         status :'finished'
     },
     {
         id: 3,
-        title :'engineering',
-        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
+        title :'Training',
+        description :'I trained in many places such as the Ministry of Communications and Information Technology in field of computer programming and networks, and in the company Orang in field of networks and i trained in field web specailly in javascript through many of websites  ',
         status :'finished'
     },
     {
         id: 4,
-        title :'engineering',
-        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
-        status :'finished'
+        title :'courses',
+        description :' I finished a course in the initiative of one million programmers in the field of full stack in python  or developing web pages  and in field data analysis and i deal with server cs50 that it responsilities development web pages by using python ',
+        status :'completed'
     },
     {
         id: 5,
-        title :'engineering',
-        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
-        status :'finished'
+        title :'projects',
+        description :'my graduation project in Al-Azhar is also using this language(python) in this field of artificial intelligence. It is an application that serves a special category so that it is responsible for controlling any device connected in labtop to this application via the control unit . The user controls the devices by clicking on the application icons not control Manually means that i used Artificial intelligence(computer vision) so the user controls the application not by manully the mouse but by moving it from the eye and pupil signal and when blink through two eyes on buttin in App will controlling in devices that around it ',
+        status :'completed'
     },
     {
         id: 6,
-        title :'engineering',
-        description :'My name is Alaa Badra I am a graduated from UNRWA. I specialized in electronic engineering and contol in computer, and in Al-Alzhar University I specialized in computer engineering and communications I got at UNRWA ranked the first on my section , So I was nominated to work for UNRWA for a year in computer engineering',
+        title :'currently',
+        description :'now i am working in gaza sky geeks in field web programming in language NodeJs , and i learnt express , React and dealing with web servers,Api ,database , and all my work on github so i have expererience in dealing github (push ,poll projects and branches) in which all this my work in team ',
         status :'active'
     },
 ]
